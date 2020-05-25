6c6273be1b2a9ae8284e551eff8589d5edcd21e0
runningwithcoffee <ecanduysak@hotmail.com>
2020-05-25 18:32:45 +0300

    WIP

--- Game/src/Paint.java
+++ Game/src/Paint.java
@@ -1,72 +1,47 @@
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionAdapter;
 
-import javax.swing.JPanel;
+import javax.swing.JComponent;
 
-public class Paint extends JPanel implements ActionListener, KeyListener, MouseListener {
-	
-	public void Paint() {
-		
-	}
+public class Paint extends JComponent implements MouseListener{
+    private Image image;
+    private Graphics2D g2d;
+    private int x,y,ox,oy;
 
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
+    public Paint(){
+        setDoubleBuffered(false);
+        addMouseListener(new MouseAdapter() {
 
-	}
+            public void mousePressed(MouseEvent e) {
+                ox=e.getX();
+                oy=.e.getY();
 
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
+                if (g2 !=null){
+                    g2d.drawLine(ox,oy,x,y);
+                    repaint();
+                    ox=x;
+                    oy=y;
+                }
+            }
+        });
+    }
+    protected void paintComponent(Graphics g){
+        if (image == null){
+            image = createImage(getSize().width,getSize().height);
+            g2d = (Graphics2D) image.getGraphics();
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+            clear();
+        }
+        g.drawImage(image,0,0,null);
+    }
+public void clear(){
+    g2d.setPaint();
+}
 }
