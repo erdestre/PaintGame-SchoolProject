125a5feea3332a54e461d335dd36cebf21dfae88
erdestre <61462201+erdestre@users.noreply.github.com>
2020-05-26 20:52:05 +0300

    Add files via upload

---
+++ Drawing.java
@@ -0,0 +1,80 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+import javax.swing.JComponent;
+
+public class Drawing extends JComponent {
+
+    // Image in which we're going to draw
+    private Image image;
+    // Graphics2D object ==> used to draw on
+    private Graphics2D g2;
+    // Mouse coordinates
+    private int X, Y, oX, oY;
+    public Color Dcolor;
+    public boolean candraw = false;
+    public Drawing() {
+        setDoubleBuffered(false);
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                oX = e.getX();
+                oY = e.getY();
+                gameScreen.setCounter();
+            }
+        });
+
+        addMouseMotionListener(new MouseMotionAdapter() {
+            public void mouseDragged(MouseEvent e) {
+                // coord x,y when drag mouse
+                X = e.getX();
+                Y = e.getY();
+
+                if (g2 != null && candraw==true) {
+                    // draw line if g2 context not null
+                    g2.setPaint(Dcolor);
+                    g2.drawLine(X, Y, oX, oY);
+                    
+                    // refresh draw area to repaint
+                    repaint();
+                    // store current coords x,y as olds x,y
+                    oX = X;
+                    oY = Y;
+                }
+            }
+        });
+    }
+
+    protected void paintComponent(Graphics g) {
+        if (image == null) {
+            // image to draw null ==> we create
+            image = createImage(getSize().width, getSize().height);
+            g2 = (Graphics2D) image.getGraphics();
+            // enable antialiasing
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            // clear draw area
+            clear();
+        }
+
+        g.drawImage(image, 0, 0, null);
+    }
+
+    // now we create exposed methods
+    public void clear() {
+        g2.setPaint(Color.white);
+        // draw white on entire draw area to clear
+        g2.fillRect(0, 0, getSize().width, getSize().height);
+        g2.setPaint(Color.black);
+        repaint();
+    }
+
+
+
+
+
+}
---
+++ gameScreen.java
@@ -0,0 +1,266 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class gameScreen extends JFrame implements KeyListener, MouseListener, ActionListener {
+	Drawing d = new Drawing();
+	Color color;
+	static int counter = 0;
+	boolean permission;
+	String Answer;
+
+	JFrame jf;
+
+	JMenuBar jpToolbar;
+	JPanel jpWhiteBoard;
+	JPanel jpMenu;
+	JPanel jpParticipant;
+	JPanel jpChat;
+	JLabel jlAnswer;
+	JPanel JpTopmenu;
+
+
+	BorderLayout blWhiteBoard;
+	BorderLayout blMainScreen;
+	BorderLayout blMenu;
+	BorderLayout blButton;
+	GridLayout glToolbar;
+
+	static JLabel jlCounter;
+
+	JTextArea participantScreen;
+	JTextArea chatScreen;
+	JTextField textField;
+
+	JButton Line, Square, Circle, Pen, selectColor, Eraser, Reset, Pass;
+	JButton Send;
+
+
+	public void MainScreen() {
+		jf = new JFrame("THE GAME THAT SHOOK THE WORLD FROM STMP STUDIOS");
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setSize(1050,600);
+		permission = Account.permission();
+		ScreenPanels();
+		jf.setVisible(true);
+
+
+	}
+	public void ScreenPanels() {
+		blMainScreen = new BorderLayout();
+		jf.setLayout(blMainScreen);
+		WhiteBoard();
+		Menu();
+
+	}
+	public void WhiteBoard() {
+
+		jpWhiteBoard = new JPanel();
+		jpWhiteBoard.setBackground(Color.white);
+		Container content = jf.getRootPane();
+		content.add(d);
+		jf.add(jpWhiteBoard,BorderLayout.NORTH);
+		jf.add(d);
+		if (permission == true) {
+			Toolbar();
+			jlAnswer = new JLabel("YOUR WORD IS:  ");
+			jpWhiteBoard.add(jlAnswer);
+			setAnswer();
+		}
+		CounterLabel();
+	}
+	public void setAnswer () {
+		Answer = getAnswer.getAnswer();
+		jlAnswer.setText("Your Word Is: " + Answer);
+	}
+	public void CounterLabel() {
+		jlCounter = new JLabel("Counter:  " + counter);
+		jpWhiteBoard.add(jlCounter);
+	}
+	public static void setCounter() {
+		counter++;
+		jlCounter.setText("Counter:  " + counter);
+	}
+	public void Toolbar() {
+		jpToolbar = new JMenuBar();
+		glToolbar = new GridLayout(1,5);
+		jpToolbar.setLayout(glToolbar);
+		jpWhiteBoard.add(jpToolbar);
+
+		Line = new JButton("Line");
+		Line.addActionListener(this);
+		Square = new JButton("Square");
+		Square.addActionListener(this);
+		Circle = new JButton("Circle");
+		Circle.addActionListener(this);
+		Pen = new JButton("Pen");
+		Pen.addActionListener(this);
+		Eraser = new JButton("Eraser");
+		Eraser.addActionListener(this);
+		selectColor = new JButton("Select Color");
+		selectColor.addActionListener(this);
+		Pass = new JButton("Pass");
+		Pass.addActionListener(this);
+
+		jpToolbar.add(Pen);
+		jpToolbar.add(Line);
+		jpToolbar.add(Square);
+		jpToolbar.add(Circle);
+		jpToolbar.add(Eraser);
+		jpToolbar.add(selectColor);
+		jpToolbar.add(Pass);
+	}
+	public void Menu() {
+		jpMenu = new JPanel();
+		JpTopmenu = new JPanel();
+		blMenu= new BorderLayout();
+		jpMenu.setLayout(blMenu);
+		jf.add(jpMenu,BorderLayout.EAST);
+		Participants();
+		ChatPanel();
+	}
+	public void Participants() {
+		jpParticipant = new JPanel();
+		jpMenu.add(jpParticipant,BorderLayout.NORTH);
+		jpParticipant.setBackground(Color.lightGray);
+		participantScreen= new JTextArea("Players:\t\t\n");
+		participantScreen.append(Source.Nickname+"\n");
+		participantScreen.setEditable(false);
+		jpParticipant.add(participantScreen);
+
+	}
+
+	public void ChatPanel() {
+		jpChat = new JPanel();
+		jpMenu.add(jpChat,BorderLayout.CENTER);
+		blButton = new BorderLayout();
+		jpChat.setLayout(blButton);
+		chatScreen = new JTextArea();
+		chatScreen.setEditable(false);
+		jpChat.setBackground(Color.lightGray);
+		jpChat.add(chatScreen);
+		textField();
+		Button();
+
+	}
+	public void Button() {
+		JButton Hand= new JButton("");
+		Image handIcon = new ImageIcon(this.getClass().getResource("HandIcon.png")).getImage();
+		Hand.setIcon(new ImageIcon(handIcon));
+		jpChat.add(Hand,BorderLayout.NORTH);
+	}
+	public void textField() {
+		textField = new JTextField();
+		jpMenu.add(textField,BorderLayout.SOUTH);
+		textField.addKeyListener(this);
+	}
+	public void AnotherRound () {
+		// Set Word
+		setAnswer();
+		// Reset Counter
+		counter = -1;
+		setCounter();
+		// Reset Screen
+		//
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == Pen) {
+			d.candraw = true;
+		}
+		else if (e.getSource() == Line) {
+			d.candraw = false;
+
+		}
+		else if (e.getSource() == Square) {
+
+		}
+		else if (e.getSource() == Circle) {
+
+		}
+		else if (e.getSource() == selectColor) {
+			color = JColorChooser.showDialog(this,"Select Color", Color.blue);
+
+
+			repaint();
+		}
+		else if (e.getSource() == Pass) {
+			AnotherRound();
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Something Went Wrong");
+		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode()==KeyEvent.VK_ENTER) {
+			if (!textField.getText().isEmpty()) {
+				chatScreen.append(Source.Nickname+": "+textField.getText()+"\n");
+				textField.setText("");
+			}
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
