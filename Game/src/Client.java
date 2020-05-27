6163d44a339116cfbf5fc2ad72c9ff6e0179169d
erdestre <61462201+erdestre@users.noreply.github.com>
2020-05-28 01:09:58 +0300

    Add files via upload

--- Game/src/Client.java
+++ Game/src/Client.java
@@ -1,6 +1,37 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-public class Client {
-	public static void main() {
-		
+public class Client extends javax.swing.JFrame{
+	static ServerSocket ss;
+	static Socket s;
+	static DataInputStream din;
+	static DataOutputStream dout;
+	static String Message;
+	public Client() {
+		;
 	}
+	public static void main() {
+		String msgin = "";
+		try {
+			ss = new ServerSocket(1201);
+			s = ss.accept();
+			din = new DataInputStream(s.getInputStream());
+			dout = new DataOutputStream(s.getOutputStream());
+			while(!msgin.equals("exit")) {
+				msgin = din.readUTF();
+				gameScreen.chatScreen.append(Message.trim()+"\n"+ msgin);
+			
+		}}
+			catch(Exception e) {
+			
+		}
+	}
+public static void msg_send(String Msg) {
+		Message = Msg;
+}
+public static void msg_receive(String Msg) {
+		Message = Msg;
+}
 }
--- Game/src/Server.java
+++ Game/src/Server.java
@@ -1,5 +1,37 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server extends javax.swing.JFrame{
-	public static void main() {
+	static ServerSocket ss;
+	static Socket s;
+	static DataInputStream din;
+	static DataOutputStream dout;
+	static String Message;
+	public Server() {
+		;
 	}
+	public static void main() {
+		String msgin = "";
+		try {
+			ss = new ServerSocket(1201);
+			s = ss.accept();
+			din = new DataInputStream(s.getInputStream());
+			dout = new DataOutputStream(s.getOutputStream());
+			while(!msgin.equals("exit")) {
+				msgin = din.readUTF();
+				gameScreen.chatScreen.append(Message.trim()+"\n"+ msgin);
+			
+		}}
+			catch(Exception e) {
+			
+		}
+	}
+public static void msg_send(String Msg) {
+		Message = Msg;
+}
+public static void msg_receive(String Msg) {
+		Message = Msg;
+}
 }
